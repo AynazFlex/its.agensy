77253a2384c717cc831885abe7ae2d629b5cb46c
AynazFlex <jkz1713x2@gmail.com>
2023-04-11 00:03:44 +0300

    step 2

--- js/nav.js
+++ js/nav.js
@@ -3,6 +3,9 @@
 const nav_burger = document.body.querySelector('.nav__burger')
 const nav_checkboxes = document.body.querySelector('.nav__checkboxes-for-mobile')
 const nav_checkboxes_wrapper = nav_checkboxes.querySelector('.nav__checkboxes-for-mobile-wrapper')
+const nav_products = document.body.querySelector('.nav__products')
+const nav_korzina = document.body.querySelector('.nav__korzina')
+const nav_products_container = nav_products.querySelector('.nav__products_container')
 
 
 nav_burger.onclick = () => {
@@ -14,6 +17,19 @@ nav_checkboxes.onclick = e => {
     if(!e.target.closest('.nav__checkboxes-for-mobile-wrapper')) {
         animEnd(nav_checkboxes_wrapper).then(() => nav_checkboxes.classList.remove('nav__active-1'))
         nav_checkboxes_wrapper.classList.remove('nav__active-2')
+    }
+}
+
+nav_korzina.onclick = () => {
+    nav_products.classList.add('nav__active-3')
+    nav_products_container.classList.add('nav__active-4')
+}
+
+nav_products.onclick = (e) => {
+    const elem = e.target
+    if(!elem.closest('.nav__products_container') || elem.closest('.nav__products_close')) {
+        animEnd(nav_products_container).then(() => nav_products.classList.remove('nav__active-3'))
+        nav_products_container.classList.remove('nav__active-4')
     }
 }
 
